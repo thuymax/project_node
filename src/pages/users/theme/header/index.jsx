d13022b1f888375d4f22fb7f6b08a40925a741a8
thuymaxx <trungthuy99xx@gmail.com>
2025-01-02 19:44:03 +0700

    thêm code

--- src/pages/users/theme/header/index.jsx
+++ src/pages/users/theme/header/index.jsx
@@ -1,28 +1,49 @@
-import {memo} from "react";
+import { memo } from "react";
+import {Link} from "react-router-dom";
 import "./style.scss";
-import { AiOutlineFacebook } from 'react-icons/ai';
-const Header = ()=>{
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail } from 'react-icons/ai';
+const Header = () => {
   return (
-    <div className="header_top"> 
-      <div className="container"> 
-          <div className="row">
-              <div className="col-6">
-                left
-              </div>
-              <div className="col-6 header_top_right">
-                 <ul>
-                    <li>
-                    <AiOutlineFacebook  />
-                    </li>
-                    <li>
-                      insta
-                    </li>
-                 </ul>
-              </div>
+    <div className="header_top">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            left
           </div>
+          <div className="col-6 header_top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <AiOutlineFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineMail />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineMail />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
 
       </div>
     </div>
   )
 };
-export default  memo(Header);
+export default memo(Header);
